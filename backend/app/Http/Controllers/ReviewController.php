7149f9d6b25761afbe516ddd710ca4f6fe823ec7
akinoringo <wbraver@outlook.jp>
2021-03-28 16:57:32 +0900

    toppage revised and create showpage

--- backend/app/Http/Controllers/ReviewController.php
+++ backend/app/Http/Controllers/ReviewController.php
@@ -9,12 +9,25 @@ class ReviewController extends Controller
 {
     //
 	public function index(){
-		return view('index');
+		$reviews = Review::where('status', 1)->orderBy('created_at', 'DESC')->paginate(9);
+
+		return view('index', compact('reviews'));
 	}
+
+	public function show($id){
+		$review = Review::where('id', $id)->where('status', 1)->first();
+
+		return view('show', compact('review'));
+	}
+
+
+
 
 	public function create(){
 		return view('review');
 	}
+
+
 
 	public function store(Request $request){
 
