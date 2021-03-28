960b87a4adc316acdee7f8b792bd00bac1ba7cb0
akinoringo <wbraver@outlook.jp>
2021-03-28 14:55:56 +0900

    add model and function of insert to db in Reviewing

--- backend/app/Http/Controllers/ReviewController.php
+++ backend/app/Http/Controllers/ReviewController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Review;
 
 class ReviewController extends Controller
 {
@@ -12,6 +13,23 @@ class ReviewController extends Controller
 	}
 
 	public function create(){
-		return view('reviews');
+		return view('review');
 	}
+
+	public function store(Request $request){
+
+		$post = $request->all();
+		$data = [
+			'user_id' => \Auth::id(),
+			'title' => $post['title'],
+			'body' => $post['body']
+		];
+
+		Review::insert($data);
+
+
+		return redirect('/');
+	}	
+
+
 }
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -16,7 +16,9 @@ Route::get('/', 'ReviewController@index')->name('index');
 Auth::routes();
 
 Route::group(['middleware'=>'auth'], function(){
-	Route::get('/review', 'ReviewController@create')->name('create');	
+	Route::get('/review', 'ReviewController@create')->name('create');
+	Route::post('/review/store', 'ReviewController@store')->name('store');
+
 });
 
 
