ee11f63383843d974e425e0c7924537c170bba4f
akinoringo <wbraver@outlook.jp>
2021-03-28 13:40:29 +0900

    create controller, routing, view of index

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -18,13 +18,21 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('css/layout.css')}}" rel="stylesheet">
+
+    <link href="{{ asset('css/utility.css')}}" rel="stylesheet">
+
+    @yield('css')
+
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-dark shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
+                    <img class="navbar-logo", src="{{ asset('images/logo.png') }}">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -72,9 +80,12 @@
             </div>
         </nav>
 
-        <main class="py-4">
+        <main class="container">
             @yield('content')
         </main>
+        <footer class="footer p20">
+            <small class="copyright">Book Reviews 2021 copyright</small>
+        </footer>
     </div>
 </body>
 </html>
